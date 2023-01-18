03ea213d56eae881c42b8e85790b0ffb07763fc4
Mihir Sharma <mihirsharma700@gmail.com>
2023-01-18 08:12:50 -0500

    Initial Commit

---
+++ dermatology-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dermatology-app
+//
+//  Created by Mihir Sharma on 12/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
